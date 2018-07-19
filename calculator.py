619e6e027d9e0dfed54a1c6fa3047c8e97b03c13
Pubudhi <pywitharana@gmail.com>
2018-07-19 23:31:01 +0530

    Commmit Day002 Practices

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
 #calculator
 
+#Day 01
 print(1+2)
 print(3)
 
 #simple arithmatics
+
 print(2*3)
 print(5-8)
 print(9/2)
 print( 5 * ( 12 - 8) + -15 )
 print( 98 + ( 59872 / ( 13 * 8)) * -51)
+
+#Day 02 - Strings
+print("Hello World")
+print('I like' + ' chocolate' + ' cake.')
+print('blink ' * 4)
+
+print(12 + 12)
+print('12' + '12')
